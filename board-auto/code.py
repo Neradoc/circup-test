248ad9e7f6452895b100a448dd7764bbb2a63478
Neradoc <neraOnGit@ri1.fr>
2021-08-08 12:47:26 +0200

    add test cases for board-auto

--- board-auto/code.py
+++ board-auto/code.py
@@ -7,4 +7,5 @@ import adafruit_binascii
 import adafruit_requests as requests
 
 from adafruit_magtag import Magtag
+from adafruit_hid.consumer_control.ConsumerControlCode import RECORD as REC
 import adafruit_esp32spi.adafruit_esp32spi_socket as socket
--- board-auto/other_code.py
+++ board-auto/other_code.py
@@ -6,3 +6,5 @@ import adafruit_nunchuk
 import adafruit_si7021
 import circuitpython_schedule
 import adafruit_magtag
+
+from adafruit_hid.consumer_control import ConsumerControl
