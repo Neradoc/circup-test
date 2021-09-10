19670b34d4bc690f230072b8b1c6a96d3c68686e
Neradoc <neraOnGit@ri1.fr>
2021-09-10 22:01:54 +0200

    add tests for bundle commands

---
+++ helper_bundles.py
@@ -0,0 +1,37 @@
+import circup
+import click
+import os
+import shutil
+import tempfile
+
+def get_save_file(token):
+    return os.path.join(tempfile.gettempdir(), f"circup_bundles_{token}.json")
+
+@click.group()
+def main():
+    pass
+
+@main.command()
+@click.argument("token", required=True, nargs=1)
+def save(token):
+    save_file = get_save_file(token)
+    print(save_file)
+    if os.path.isfile(circup.BUNDLES_CONFIG_LOCAL):
+        shutil.copy(circup.BUNDLES_CONFIG_LOCAL, save_file)
+
+@main.command()
+@click.argument("token", required=True, nargs=1)
+def restore(token):
+    save_file = get_save_file(token)
+    if os.path.isfile(save_file):
+        shutil.copy(save_file, circup.BUNDLES_CONFIG_LOCAL)
+
+@main.command()
+@click.argument("token", required=True, nargs=1)
+def clean(token):
+    save_file = get_save_file(token)
+    if os.path.isfile(save_file):
+        os.unlink(save_file)
+
+if __name__ == "__main__":
+    main()
---
+++ test_bundles.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+DIR=`dirname $0`
+TOKEN=`uuidgen`
+#
+echo -e "#####################################################################"
+echo -e "# Warning this test suite will modify your bundles list !"
+while true; do
+	read -p "# Do you wish to continue ?" yn
+	case $yn in
+		[Yy]* ) break;;
+		[Nn]* ) exit;;
+		* ) echo "Please answer yes or no.";;
+	esac
+done
+#
+echo -e "\n# Here are your bundles ###########################################\n"
+circup bundle-show
+#
+python3 "$DIR/helper_bundles.py" save "$TOKEN"
+#
+echo -e "\n# Invalid bundle path ###########################################\n"
+circup bundle-add toto
+#
+echo -e "\n# Non existing bundle ###########################################\n"
+circup bundle-add Neradoc/NOTEXISTS
+#
+echo -e "\n# Not actually a bundle ###########################################\n"
+circup bundle-add Neradoc/circup
+#
+echo -e "\n# Not actually a bundle ###########################################\n"
+circup bundle-add Neradoc/circup
+#
+echo -e "\n# Reset bundles ###########################################\n"
+circup bundle-remove --reset
+#
+echo -e "\n# Remove Layouts for test ###########################################\n"
+circup bundle-remove Neradoc/Circuitpython_Keyboard_Layouts
+#
+echo -e "\n# Add back Layouts ###########################################\n"
+circup bundle-add Neradoc/Circuitpython_Keyboard_Layouts
+#
+echo -e "\n# Remove Layouts #########################################\n"
+circup bundle-remove Neradoc/Circuitpython_Keyboard_Layouts
+#
+echo -e "\n# Show all of that ###########################################\n"
+circup bundle-show
+#
+python3 "$DIR/helper_bundles.py" restore "$TOKEN"
+python3 "$DIR/helper_bundles.py" clean "$TOKEN"
+#
